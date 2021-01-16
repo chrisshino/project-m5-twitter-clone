9dc4b1551ab49e767d04291088eb78cd30c34ac6
chris shino <chris.shino@hotmail.com>
2021-01-16 15:59:13 -0500

    done the sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import styled from 'styled-components'
 import HomeFeed from './components/HomeFeed'
 import Notifications from './components/Notifications'
 import Bookmarks from './components/Bookmarks'
 import TweetDetails from './components/TweetDetails'
 import Profile from './components/Profile'
-
+// import Sidebar from '.components/Sidebar'
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,30 +13,41 @@ import {
   Link,
   BrowserRouter
 } from "react-router-dom";
+import Sidebar from './components/Sidebar'
 
 
 function App() {
   return (
    <BrowserRouter>
-    <Switch>
-      <Route exact path='/'>
-        <HomeFeed />
-      </Route>
-      <Route exact path='/notifications'>
-        <Notifications/>
-      </Route>
-      <Route exact path ='bookmarks'>
-        <Bookmarks/>
-      </Route>
-      <Route exact path = '/tweet/:tweetId'>
-        <TweetDetails/>
-      </Route>
-      <Route exact path='/:profileId'>
-        <Profile/>
-      </Route>
-    </Switch>
+
+    <Main>
+      <Sidebar/>
+      <Switch>
+        <Route exact path='/'>
+          <HomeFeed />
+        </Route>
+        <Route exact path='/notifications'>
+          <Notifications/>
+        </Route>
+        <Route exact path ='/bookmarks'>
+          <Bookmarks/>
+        </Route>
+        <Route exact path = '/tweet/:tweetId'>
+          <TweetDetails/>
+        </Route>
+        <Route exact path='/:profileId'>
+          <Profile/>
+        </Route>
+      </Switch>
+    </Main>
    </BrowserRouter>
   );
 }
+
+
+const Main = styled.div`
+  display: flex;
+
+`;
 
 export default App;
---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { NavLink } from "react-router-dom"
+import styled from 'styled-components'
+import { ReactComponent as Logo } from '../assets/logo.svg'
+import  {COLORS}  from '../../src/constants'
+import { FiHome, FiUser, FiBell, FiBookmark } from "react-icons/fi";
+function Sidebar() {
+  return (
+    <Wrapper>
+      <Logo style={{width: '45px', height:'60px'}}/>
+      <StyledNavLink to='/'>
+        <Container>
+          <FiHome style={{fontSize: '1.3rem'}}/>
+          <Text>Home</Text>
+        </Container>
+      </StyledNavLink>
+
+
+      <StyledNavLink to='/:profileId'>
+        <Container>
+          <FiUser style={{fontSize: '1.3rem'}}/>
+          <Text>Profile</Text>
+        </Container>
+      </StyledNavLink>
+
+      <StyledNavLink to='/notifications'>
+        <Container>
+          <FiBell style={{fontSize: '1.3rem'}}/>
+          <Text>Notifications</Text>
+        </Container>
+      </StyledNavLink>
+
+      <StyledNavLink to='/bookmarks'>
+        <Container>
+          <FiBookmark style={{fontSize: '1.3rem'}}/>
+          <Text>Bookmarks</Text>
+        </Container>
+      </StyledNavLink>
+    </Wrapper>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  padding: 0.5rem 0.8rem;
+  align-items: center;
+  
+`;
+
+const Text = styled.div`
+  margin-left: 1rem;
+  font-weight: bold;
+  padding-top: 2%;
+  padding-bottom: 2%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* width: 20vw; */
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  font-size: 1rem;
+  margin-top: 0.5rem;
+  color: black;
+  font-family: sans-serif;
+  &.active {
+    color: ${COLORS.primary}
+  }
+  &:hover {
+    background-color: #E9D3ff;
+    border-radius: 25px;
+    color: ${COLORS.primary};
+  }
+`;
+
+
+export default Sidebar
