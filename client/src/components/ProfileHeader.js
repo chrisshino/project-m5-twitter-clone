2e7ef6d29b215882ed705b14241484fcfc33020b
chris shino <chris.shino@hotmail.com>
2021-01-23 09:13:05 -0500

    almost finished profile header

--- client/src/components/ProfileHeader.js
+++ client/src/components/ProfileHeader.js
@@ -20,19 +20,113 @@ function ProfileHeader() {
   }
   return (
     <div>
-      <ProfileBanner src={profileInfo.profile.bannerSrc}/>
-      <ProfileImageAndButtonContainer>
+      <BannerProfileButtonContainer>
+        <ProfileBanner src={profileInfo.profile.bannerSrc}/>
         <ProfileImageLarge src={profileInfo.profile.avatarSrc}/>
-        <FollowingButton>Following</FollowingButton>
-      </ProfileImageAndButtonContainer>
+        <ButtonContainer>
+          <FollowingButton>Following</FollowingButton>
+        </ButtonContainer>
+      </BannerProfileButtonContainer>
+      <UserInfoContainer>
+        <DisplayName>{profileInfo.profile.displayName}</DisplayName>
+        <HandleNameContainer>
+          <UserHandleName>@{profileInfo.profile.handle}</UserHandleName>
+          <FollowingYouContainer>Follows You</FollowingYouContainer>
+        </HandleNameContainer>
+        <p>{profileInfo.profile.bio}</p>
+        <CityAreaContainer>
+          <CityContainer>{profileInfo.profile.location}</CityContainer>
+          <JoinedDate>{profileInfo.profile.joined}</JoinedDate>
+        </CityAreaContainer>
+        <FollowerContainer>
+          <Following>{profileInfo.profile.numFollowing} Following</Following>
+          <Followers>{profileInfo.profile.numFollowers} Followers</Followers>
+        </FollowerContainer>
+      </UserInfoContainer>
     </div>
   )
 }
 
+const Following = styled.div`
+  /* color: grey; */
+  font-size: 0.8rem;
+  margin-right: 1rem;
+`;
+
+const Followers = styled.div`
+  /* color: grey; */
+  font-size: 0.8rem;
+`;
+
+const FollowerContainer = styled.div`
+  display: flex;
+  margin-top: 0.5rem;
+`;
+
+
+const JoinedDate = styled.div`
+  font-size: 0.8rem;
+  color: grey;
+`;
+const CityContainer = styled.div`
+  font-size: 0.8rem;
+  color: grey;
+  margin-right: 1rem;
+`;
+
+const CityAreaContainer = styled.div`
+  display: flex;
+`;
+
+const FollowingYouContainer = styled.div`
+  color: grey;
+  font-size: 0.8rem;
+  background-color: whitesmoke;
+  border-radius: 5px;
+`;
+
+const HandleNameContainer = styled.div`
+  display: flex;
+  margin: none;
+`;
+
+const UserHandleName = styled.div`
+  color: grey;
+  font-size: 0.8rem;
+  margin-right: 10px;
+`;
+
+const DisplayName = styled.div`
+  font-weight: bolder;
+  font-size: 1rem;
+  margin: none;
+`;
+
+const UserInfoContainer = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  font-family: sans-serif;
+`;
+
+const BannerProfileButtonContainer = styled.div`
+  position: relative;
+  margin-bottom: 4rem;
+`;
+
 const ProfileImageLarge = styled.img`
   border-radius: 50%;
   width: 20%;
-  outline: 1px solid white;
+  border: 3px solid white;
+  position: absolute;
+  top: 60%;
+  left: 3%;
+`;
+
+const ButtonContainer = styled.div`
+display: flex;
+justify-content: flex-end;
+padding-top: 1rem;
+padding-right: 1rem;
 `;
 
 const FollowingButton = styled.button`
@@ -45,16 +139,9 @@ const FollowingButton = styled.button`
   color: white;
 `;
 
-const ProfileImageAndButtonContainer = styled.div`
-  /* position: absolute; */
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  align-items: baseline;
-`;
-
 const ProfileBanner = styled.img`
   width: 100%;
+  position: relative;
 `;
 
 
